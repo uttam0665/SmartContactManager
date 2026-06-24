ad248445c1c921aa53565863e7c3b09315c10912
uttam gouda <122440492+uttam0665@users.noreply.github.com>
2026-06-25 02:45:52 +0530

    Add Validation and show proper messsage

---
+++ src/main/java/com/uttam/helper/Message.java
@@ -0,0 +1,27 @@
+package com.uttam.helper;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class Message {
+	
+	private String content;
+	
+	@Builder.Default
+	private MessageType type=MessageType.blue;
+
+	
+	
+	
+}
---
+++ src/main/java/com/uttam/helper/MessageType.java
@@ -0,0 +1,6 @@
+package com.uttam.helper;
+
+public enum MessageType {
+
+	blue,green,red,yellow
+}
