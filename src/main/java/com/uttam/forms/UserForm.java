aee167695b2f6fd6512da96a5d612f0efcb34bdb
uttam gouda <122440492+uttam0665@users.noreply.github.com>
2026-06-25 02:43:02 +0530

    Add registion form data in Database

---
+++ src/main/java/com/uttam/forms/UserForm.java
@@ -0,0 +1,42 @@
+package com.uttam.forms;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class UserForm {
+
+	@NotBlank(message = "Username is required")
+	@Size(min = 3,message = "Min 3 Characters is required")
+	private String name;
+	
+	@Email(message = "Invalid Email Address")
+	@NotBlank(message = "Email is required")
+	private String email;
+	
+	@NotBlank(message = "Password is required")
+	@Size(min=6,max = 15,message ="Min 6 and Max 15 character is required" )
+	private String password;
+	
+	@Size(min = 8,max = 12,message = "Invalid Phone Number")
+	private String phoneNumber;
+	
+	@NotBlank(message = "About is required")
+	private String about;
+	
+
+	
+	
+}
